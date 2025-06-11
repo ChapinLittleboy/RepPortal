16538409c9823174f32cc26cec4ab9fc9b948bbe
littleboy1442 <51123828+littleboy1442@users.noreply.github.com>
2025-06-11 10:24:06 -0400

    Add React version of RepPortal with basic features

---
+++ RepPortalReact/src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h3>Welcome to the Chapin Rep Portal</h3>
+      <p>Please log in to continue your session.</p>
+    </div>
+  );
+}
