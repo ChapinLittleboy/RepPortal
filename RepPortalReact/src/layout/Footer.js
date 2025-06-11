2f050debf651ee7449a036b3d6cd34aabb912af1
littleboy1442 <51123828+littleboy1442@users.noreply.github.com>
2025-06-11 11:51:39 -0400

    Delete the RepPortalReact folder from this project

--- RepPortalReact/src/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer className="bg-light text-center text-muted py-3 mt-auto">
-      <small>&copy; 2024 RepPortal</small>
-    </footer>
-  );
-}
