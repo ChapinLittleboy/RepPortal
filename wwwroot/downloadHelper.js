ea95efcfef5467b900cf05aa3efe50ab5eff21f3
littleboy1442 <51123828+littleboy1442@users.noreply.github.com>
2025-08-06 12:54:30 -0400

    Fixed Download Contacts link

---
+++ wwwroot/downloadHelper.js
@@ -0,0 +1,9 @@
+﻿function downloadFile(filename, contentType, data) {
+    const blob = new Blob([new Uint8Array(data)], { type: contentType });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
